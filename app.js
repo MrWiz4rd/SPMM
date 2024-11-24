8e4a69cc5f0368ee85340bc27b71101ceb58136d
Roman <77737080+MrWiz4rd@users.noreply.github.com>
2024-11-25 00:06:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-// Firebase konfigurácia
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAx6FWn6zc36lnMcnk8aYYH4FvCxcIrC6o",
-    authDomain: "spmm-a8fa5.firebaseapp.com",
-    databaseURL: "https://spmm-a8fa5.firebaseio.com",
-    projectId: "spmm-a8fa5",
-    storageBucket: "spmm-a8fa5.appspot.com",
-    messagingSenderId: "962431433976",
-    appId: "1:962431433976:web:d66c57740a5675e5e58f0d",
-    measurementId: "G-2K3YFBQKGE"
+  apiKey: "AIzaSyAx6FWn6zc36lnMcnk8aYYH4FvCxcIrC6o",
+  authDomain: "spmm-a8fa5.firebaseapp.com",
+  projectId: "spmm-a8fa5",
+  storageBucket: "spmm-a8fa5.firebasestorage.app",
+  messagingSenderId: "962431433976",
+  appId: "1:962431433976:web:d66c57740a5675e5e58f0d",
+  measurementId: "G-2K3YFBQKGE"
 };
 
-// Inicializácia Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const analytics = firebase.analytics(app);
-const database = firebase.database();
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Referencie pre signalizáciu
 const offerRef = database.ref('/offer');
 const answerRef = database.ref('/answer');
